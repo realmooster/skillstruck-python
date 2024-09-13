310e89f6e5ee96a5fde3c2c042f977d83be12ee7
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:49 -0700

    exported from Skill Struck

---
+++ Python 3/reading_partsFILES.py
@@ -0,0 +1,36 @@
+# Code Checkpoint
+
+# file = open("example.txt", "r")
+# print(file.read(10))
+# print(file.readline())
+# print(file.readline())
+# file.close()
+
+# ---------------------------------------------------------
+
+# Challenge 1
+
+# number = int(input("How many numbers do u want"))
+
+# file = open('example.txt', 'r')
+# print(file.read(number))
+# file.close()
+
+# ---------------------------------------------------------
+
+# Challenge 2
+
+# file = open('example.tvt' , 'r')
+# print(file.read())
+
+# hello = file.split()
+# print(len(hello))
+# file.close()
+
+# ---------------------------------------------------------
+
+# Challenge 3
+
+# file = open('example.tvt' , 'r')
+# print(len(file.readlines()))
+# file.close()
