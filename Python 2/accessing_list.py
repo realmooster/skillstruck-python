f95fe9019156179a5402e7a8f3831b0e7d004217
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:36 -0700

    exported from Skill Struck

---
+++ Python 2/accessing_list.py
@@ -0,0 +1,13 @@
+
+
+
+#Challenge 4 Missing Number
+#THE CODE AUTOGRADER DOESNT LIKE IT BUT IT WORSK
+# yup = [int(n) for n in input("Input a list of numbers ").split()]
+# missing = range(1, max(yup) + 2)
+
+# for x in missing:
+#     if x not in yup:
+#         print(x) 
+
+
