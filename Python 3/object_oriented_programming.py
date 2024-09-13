92c9d5d6c843eee2646a8d7cdd25fb094cae3827
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:53 -0700

    exported from Skill Struck

---
+++ Python 3/object_oriented_programming.py
@@ -0,0 +1,12 @@
+# Code Checkpoint
+
+# class Dog:
+
+#     def __init__(self, name, color, size):
+#         self.name = name
+#         self.color = color
+#         self.size = size
+
+# ---------------------------------------------------------
+# Challenge 1
+
