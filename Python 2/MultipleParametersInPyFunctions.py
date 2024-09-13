b56e9d50bfccc5f83b9725ea47389e140ff6d3af
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:40 -0700

    exported from Skill Struck

---
+++ Python 2/MultipleParametersInPyFunctions.py
@@ -0,0 +1,116 @@
+# Code Checkpoint
+
+# def ily(ph, ph2):
+#     print("hola amigo " + ph)
+#     print("hola amigo " + ph2)
+# ily("u are fake", "ur silly")
+# ily("i luv u", "i dont luv u")
+
+# --------------------------------------------------------
+
+# Challenge 1
+
+# tehe = input("fruit: ")
+# nottehe = input("fruit: ")
+# hehe = input("fruit: ")
+
+# def fruit(ily, ihy, lol):
+#     print(ily + ihy + lol)
+# fruit(tehe, nottehe, hehe)
+
+# --------------------------------------------------------
+
+# Challenge 2
+
+# num = int(input("gimme a num: "))
+# boohoo = int(input("gimme a num: "))
+# yippee = []
+# def airpods(airhead,airhead2):
+#     yippee.append(airhead)
+#     yippee.append(airhead2)
+#     print(min(yippee))
+# airpods(num,boohoo)
+
+# --------------------------------------------------------
+
+# Challenge 3
+
+# donotfear = int(input(": "))
+# poopoo = int(input(": "))
+# peepee = int(input(": "))
+
+# def suck(yay, notyay, usuck):
+#     if yay == notyay == usuck:
+#         print("3")
+#     elif yay == notyay or yay == usuck or usuck == notyay:
+#         print("2")
+#     else:
+#         print("0")
+# suck(donotfear, poopoo, peepee)
+
+# --------------------------------------------------------
+
+# Challenge 4
+
+# var1 = int(input(": "))
+# var2 = int(input(": "))
+# var3 = int(input(": "))
+# var4 = int(input(": "))
+# var5 = int(input(": "))
+# pop = []
+# def yippee(hi, ho, hp, hl, hk):
+#     pop.append(hi)
+#     pop.append(ho)
+#     pop.append(hp)
+#     pop.append(hl)
+#     pop.append(hk)
+#     print(min(pop))
+# yippee(var1, var2, var3, var4, var5)
+
+# --------------------------------------------------------
+
+# Challenge 5
+
+# thing = int(input(": "))
+# thing2 = int(input(": "))
+# thing3 = int(input(": "))
+# lis = []
+# def oddish(a,b,c):
+#     lis.append(a)
+#     lis.append(b)
+#     lis.append(c)
+#     if a == c:
+#         print("2")
+#     elif b == c:
+#         print("1")
+#     elif a == b:
+#         print("3")
+
+
+# oddish(thing,thing2,thing3)
+
+# --------------------------------------------------------
+
+# Challenge 6
+
+# kys = int(input("month 1-12: " ))
+# donotkys = int(input("day 1-31: " ))
+
+# def things(month, date):
+#     if month == 4 or month == 6 or month == 9 or month == 11:
+#         if date == 30:
+#             print(month + 1, 1)
+#         else:
+#             print(month, date + 1)
+#     elif month == 2:
+#         if date == 28:
+#             print(month + 1, 1)
+#         else:
+#             print(month, date + 1)
+#     else:
+#         if date == 31:
+#             print(month + 1, 1)
+#         else:
+#             print(month, date + 1)
+
+# things(kys, donotkys)
