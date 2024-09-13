10d3b212bec7780a6e955dd6f76d8557b51bc42b
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:48 -0700

    exported from Skill Struck

---
+++ Python 3/readingfiles.py
@@ -0,0 +1,21 @@
+# Code Checkpoint
+
+# file = open("kys.txt", "r")
+# print(file.read(10))
+# print(file.readline())
+# print(file.readline())
+# file.close()
+
+# --------------------------------------------------------
+
+# # Challenge 1
+
+# cobra = open("cobra.txt", "r")
+# print(cobra.read())
+
+# ---------------------------------------------------------
+
+# Challenge 2
+
+# speech = open("speech.txt", "r")
+# print(speech.read())
