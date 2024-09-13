1d6df1527e03ef2ad3e371017df204db69a51547
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:39 -0700

    exported from Skill Struck

---
+++ Python 2/Python Functions.py
@@ -0,0 +1,71 @@
+# Code Checkpoint
+
+# def race():
+#     print("jimmy won the race")
+
+# race()
+
+# --------------------------------------------------------
+
+# Challenge 1
+
+# def charlie():
+#     print("bro charlie is comp")
+# charlie()
+
+# --------------------------------------------------------
+
+# Challenge 2
+
+# def game():
+#     hey = input("")
+#     if hey == "yes":
+#         print("Initialization Complete")
+#     elif hey == "no":
+#         print("Initialization Failed")
+
+# game()
+
+# --------------------------------------------------------
+
+# Challenge 3
+
+# chickens = 5
+# eggs_per_week = 9
+
+# def farmer():
+#     print(chickens * eggs_per_week)
+# farmer()
+
+# --------------------------------------------------------
+
+# Challenge 4
+
+# hola = int(input("integer: "))
+
+# def something():
+#     print("Hello"*hola)
+# something()
+
+# --------------------------------------------------------
+
+# Challenge 5
+
+# def yuh():
+#     heyy = input("a fruit: ")
+#     print(heyy*3)
+# yuh()
+
+# --------------------------------------------------------
+
+# Challenge 6
+
+length = 6
+
+def surface_area():
+    print(length * length * 6)
+surface_area()
+
+def volume():
+    print(length * length * length)
+volume()
