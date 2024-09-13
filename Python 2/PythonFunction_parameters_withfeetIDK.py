4d7d8fe5170adf75b56dbfcf0ee78992507e6ad2
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:39 -0700

    exported from Skill Struck

---
+++ Python 2/PythonFunction_parameters_withfeetIDK.py
@@ -0,0 +1,85 @@
+# Code Checkpoint
+
+# def heyy(yippee):
+#     print("i luv u. " + yippee)
+# heyy("just kidding!") <---- replaces yippee with string
+# / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+# def word(idk):
+#     print("u are really " + idk)
+# word("cool")
+# word("dumb")
+# word("silly")
+# --------------------------------------------------------
+
+# Challenge 1
+
+# hello = input("fruit: ")
+# def yuh(thing):
+#     print(thing + thing + thing)
+# yuh(hello)
+
+# --------------------------------------------------------
+
+# Challenge 2
+
+# month = int(input("Which month?  1-12"))
+
+# def invitation (choice) :
+# 	if choice == 1 or choice == 3 or choice == 5 or choice == 7 or choice == 8 or choice == 10 or choice == 12:
+# 		print(31)
+# 	elif choice == 2:
+# 		print(28)
+# 	else:
+# 		print(30)
+
+# invitation(month) 
+
+# --------------------------------------------------------
+
+# Challenge 3
+
+# def odd_even(box):
+#     if box % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
+# odd_even(67)
+# odd_even(69)
+# odd_even(16)
+
+# --------------------------------------------------------
+
+# Challenge 4
+
+# ello = input(": ")
+
+# def nj(c):
+#     if "c" in ello or "C" in ello:
+#         print(c + " can be a part of the party!")
+#     else:
+#         print("Sorry! " + c + " doesn't start with the letter c!")
+# nj(ello)
+
+# --------------------------------------------------------
+
+# Challenge 5
+
+# kys = int(input(": "))
+
+# def pooks(aye):
+#     if aye % 4 == 0 and aye % 100 != 0: <------ the != means that it compares two values to being "not equal"
+#         print("Leap year!")
+#     else:
+#         print("Not a leap year.")
+# pooks(kys)
+
+# --------------------------------------------------------
+
+# Challenge 6
+
+# hispooks = int(input("radius or something: "))
+
+# def littlespoon(pookie):
+#     thisone = (pookie * pookie * 3.14159)
+#     print(round(thisone, 1))
+# littlespoon(hispooks)
