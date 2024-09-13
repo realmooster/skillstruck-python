5288968fcc9f1a0ce2f5005458e7288cebe299f2
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:35 -0700

    exported from Skill Struck

---
+++ Python 2/if_for.py
@@ -0,0 +1,12 @@
+# letter = input("Put a letter lil bro")
+# my_list = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white", "gray", "pink", "indigo", "brown", "tan", "gold", "silver"]
+
+# hola = "There are {} items in the list that have the letter {} in it."
+
+# total = 0
+
+# for x in my_list:
+#     if letter in x:
+#         total += 1
+# print(hola.format(total, letter))
+#adds the instances of "letter"
