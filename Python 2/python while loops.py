fd7d7cc59985f807ebcbe83a9b485d17000e4adc
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:38 -0700

    exported from Skill Struck

---
+++ Python 2/python while loops.py
@@ -0,0 +1,78 @@
+# Code Checkpoint
+# number = 1
+
+# while number <= 50:
+#     print(number)
+#     number += 1
+
+# --------------------------------------------------------
+
+#Challenge 1
+
+# total = 0   
+# thingy = int(input()) 
+# while thingy != 0: <----- sets the 2 values till they are equal to 0
+#     total += 1
+#     thingy = int(input())  <----------- redo the input so it keeps asking the user till he puts a 0
+# print(total)
+
+# --------------------------------------------------------
+
+# Challenge 2
+
+# total = 0
+# sumlist = int(input())
+
+# while sumlist != 0: <----- continues thing until 0 is inputted by "said user"
+ 
+#     total += sumlist <------- adds the sumlist input to the value "total" (0)
+
+#     sumlist = int(input())
+# print(total)
+
+# --------------------------------------------------------
+
+#  Challenge 3
+
+# yeah = int(input())
+# yessir = [] <------- create a list for values
+
+# while yeah != 0:
+#     if yeah > 0:
+#         yessir.append(yeah)
+#         yeah = int(input())
+# print(max(yessir)) <------ takes the biggest value from the list to finish.
+
+# --------------------------------------------------------
+
+# Challenge 4
+
+# total = 0 <------ variable for later sum
+# thisone = 0 <-------- total num of numbers
+# ello = int(input())
+# while ello != 0:
+#     thisone += 1 <-------- total num of numbers
+#     total += ello <-------- sum of numbers
+#     ello = int(input())
+# print(float(total / thisone))
+
+# --------------------------------------------------------
+
+# Challenge 5
+
+# yippee = int(input("day1 : "))
+# nope = int(input("raceday : "))
+
+# totaldays = 0 <----- does the first distance for day 1
+
+# while yippee <= nope:
+#     yippee *= 1.1 <-------------- finds the value of 10% increase for each day
+#     totaldays += 1 <--- changes the value for each day
+
+# totaldays += 1 <---------- add one to include day 1
+# print(totaldays)
+
+# --------------------------------------------------------
+
+# Challenge 6
+
