05f5008ca9b1dda8a5c42c0243c0bb55567354a6
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:43 -0700

    exported from Skill Struck

---
+++ Python 2/Tuples.py
@@ -0,0 +1,34 @@
+# Code Checkpoint
+
+# grades = (95, 70, 85, 92, 100)
+
+# print(grades)
+# print(grades[-2])
+# print(grades[0:3])
+
+# --------------------------------------------------------
+
+# Challenge 1
+
+# friends = (3, 5, 7, 8, 10, 2, 12, 4)
+
+# ask = int(input(": "))
+# if ask in friends:
+#     print("Yes")
+# else:
+#     print("No")
+
+# --------------------------------------------------------
+# Challenge 2
+
+# food = (3, 5, 7, 8, 10, 2, 12, 4)
+
+# print(food[-3])
+# print(food[5])
+# print(food[0:4])
+
+# --------------------------------------------------------
+# Challenge 3
+# candies = (3, 5, 7, 8, 10, 2, 50, 4, 29, 14)
+
+# print(candies[-4:-1])
