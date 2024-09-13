4b985e30d80d54e12d9d6bf9615269c9f6f0098f
alex.dinhh <zewhooshmaster@gmail.com>
2024-09-13 12:06:41 -0700

    exported from Skill Struck

---
+++ Python 2/python_dictionaries.py
@@ -0,0 +1,62 @@
+# Code Checkpoint
+
+# mountains = {"Timpanogos" : 5, "Everest": 10, "Kilimanjaro": 6, "Vesuvius": 7}
+# mountains["Kilimanjaro"] = 10
+# print(mountains)
+# print(mountains["Vesuvius"])
+# mountains["Kilimanjaro"] = 10
+
+# # --------------------------------------------------------
+
+# # Challenge 1
+
+# kys = {"pop" : 5, "second" : 6, "third" : 7, "fourth" : 8, "fifth" : 9, "sixth" : 10, "seventh" : 11, "twelveth" : 12, "eight" : 13, "nine" : 14}
+# print(kys) 
+
+# # --------------------------------------------------------
+
+# Challenge 2
+
+# tehe = {"one" : 1, "two" : 2, "three" : 4, "five" : 5}
+# thisone = ["hi", "hey", "na", "ya", "idk"]
+# print(tehe)
+# print(thisone)
+
+# # --------------------------------------------------------
+
+# Challenge 3
+
+# heyy = int(input(": " ))
+# hi = int(input(": " ))
+# hii = int(input(": " ))
+# hola = int(input(": " ))
+# hallo = int(input(": " ))
+
+# thisone = {"knife": heyy, "fire starter" : hi, "pot" : hii, "rope" : hola, "tarp" : hallo}
+
+# print(thisone)
+
+# --------------------------------------------------------
+
+# Challenge 4
+
+# colors = { "red": 4, "orange": 1, "yellow": 2, "green": 10, "blue": 5, "purple": 6}
+
+# print(colors["orange"])
+# print(colors["purple"])
+
+# colors["red"] = 9
+# colors["blue"] = 69
+
+# print(colors)
+
+# --------------------------------------------------------
+
+# Challenge 5
+
+# pets = {"Fish" : 30, "Dogs": 2, "Chickens": 5, "Cats": 2, "Bunnies": 1}
+
+# pets["Fish"] = 20
+# pets["Bunnies"] = 7
+# print("Because 10 fish died, and the bunny had 6 babies, you now have " + str(pets["Fish"]) + " fish and " + str(pets["Bunnies"]) + " bunnies at your house.")
+	
